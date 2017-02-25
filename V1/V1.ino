9158231616b055d3a61290bc4e58b4211b1ea28a
Eric Miller <eric@legoaces.org>
2017-02-25 15:02:31 -0500

    Added library for servos with range constraints.

--- V1/V1.ino
+++ V1/V1.ino
@@ -7,21 +7,23 @@
  http://www.arduino.cc/en/Tutorial/Sweep
 */
 
-#include <Servo.h>
+#include "myservo.h"
 
-Servo myservo;  // create servo object to control a servo
+
+MyServo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
 int pos = 0;    // variable to store the servo position
 
 void setup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.calibrate(90,10);
 }
 
 void loop() {
-  myservo.write(5);
+  myservo.write(0);
   delay(1000);
-  myservo.write(155);
+  myservo.write(1.0);
   delay(1000);
 //  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
 //    // in steps of 1 degree
